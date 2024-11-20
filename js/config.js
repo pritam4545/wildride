bc7b8b784f579f8d211f7ab0bc8fc5ea99d86169
pritam4545 <phadtarepritam43@gmail.com>
2024-11-20 15:26:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_bUGUjYrSl', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5ngnpaqjddqhjqlc8d2r6m6e0r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'ap-south-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
